b762bee70bfa0de669d2c986f016ee90ed5ba546
natalyapeixoto <natalya_peixoto@hotmail.com>
2021-09-09 08:58:49 -0300

    upload

---
+++ src/exercise/01.js
@@ -0,0 +1,39 @@
+// Code splitting
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+const Globe = React.lazy(() => import(/* webpackPrefetch: true */ '../globe'))
+
+function App() {
+  const [showGlobe, setShowGlobe] = React.useState(false)
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100%',
+        padding: '2rem',
+      }}
+    >
+      <label style={{marginBottom: '1rem'}}>
+        <input
+          type="checkbox"
+          checked={showGlobe}
+          onChange={e => setShowGlobe(e.target.checked)}
+        />
+        {' show globe'}
+      </label>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <div style={{width: 400, height: 400}}>
+          {showGlobe ? <Globe /> : null}
+        </div>
+      </React.Suspense>
+    </div>
+  )
+}
+
+export default App
